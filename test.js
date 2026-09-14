19e6549abcd2b1a1a59bc080116ece402c044909
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-09-14 10:54:03 +0200

    Fix directory asset imports (#5)

--- test.js
+++ test.js
@@ -13,6 +13,73 @@ test('basic', (t) => {
   )
 })
 
+test('directory', (t) => {
+  t.is(requireAsset('./fixtures', pathToFileURL('./test/')), path.join(__dirname, 'test/fixtures'))
+})
+
+test('directory with trailing separator', (t) => {
+  t.is(requireAsset('./fixtures/', pathToFileURL('./test/')), path.join(__dirname, 'test/fixtures'))
+})
+
+test('directory of the referrer', (t) => {
+  t.is(
+    requireAsset('./', pathToFileURL('./test/fixtures/asset.txt')),
+    path.join(__dirname, 'test/fixtures')
+  )
+})
+
+test('directory of the referrer without trailing separator', (t) => {
+  t.is(
+    requireAsset('.', pathToFileURL('./test/fixtures/asset.txt')),
+    path.join(__dirname, 'test/fixtures')
+  )
+})
+
+test('directory that does not exist', (t) => {
+  t.exception(() => requireAsset('./nope/', pathToFileURL('./test/fixtures/')))
+})
+
+test('nested directory', (t) => {
+  t.is(
+    requireAsset('./directory', pathToFileURL('./test/fixtures/')),
+    path.join(__dirname, 'test/fixtures/directory')
+  )
+})
+
+test('file in a nested directory', (t) => {
+  t.is(
+    requireAsset('./directory/nested.txt', pathToFileURL('./test/fixtures/')),
+    path.join(__dirname, 'test/fixtures/directory/nested.txt')
+  )
+})
+
+test('parent directory', (t) => {
+  t.is(
+    requireAsset('..', pathToFileURL('./test/fixtures/directory/')),
+    path.join(__dirname, 'test/fixtures')
+  )
+})
+
+test('referrer as a path', (t) => {
+  t.is(
+    requireAsset('./asset.txt', path.join(__dirname, 'test/fixtures/index.js')),
+    path.join(__dirname, 'test/fixtures/asset.txt')
+  )
+})
+
+test('asset that does not exist', (t) => {
+  try {
+    requireAsset('./nope.txt', pathToFileURL('./test/fixtures/'))
+
+    t.fail('should have thrown')
+  } catch (err) {
+    t.is(err.code, 'ASSET_NOT_FOUND')
+    t.is(err.specifier, './nope.txt')
+    t.ok(err.referrer)
+    t.ok(Array.isArray(err.candidates))
+  }
+})
+
 test('bundle', (t) => {
   const bundle = new Bundle()
 
@@ -88,3 +155,23 @@ function withRequireAsset(bundle) {
     '/node_modules/require-asset'
   )
 }
+
+test('bundle with a directory', (t) => {
+  const bundle = new Bundle()
+
+  withRequireAsset(bundle)
+
+  bundle.write('/index.js', "module.exports = require('require-asset')('./', __filename)", {
+    main: true,
+    imports: {
+      './': {
+        asset: '/fixtures/'
+      }
+    }
+  })
+
+  t.is(
+    evaluate(bundle.mount(pathToFileURL('./test/'))).exports,
+    path.join(__dirname, 'test/fixtures') + path.sep
+  )
+})
